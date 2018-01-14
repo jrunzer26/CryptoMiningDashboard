35e75afcf298f53ff197a48bc3ae60771eba4e3d
jrunzer26 <jason.runzer@uoit.net>
2018-01-14 17:30:40 -0500

    added rig monitoring

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -5,16 +5,53 @@ function dashboard() {
 	$("#dashboard").show();
 	$("#mining").hide();
 	$("#portfolio").hide();
+	$("#status").hide();
 }
 
 function mining() {
 	$("#dashboard").hide();
 	$("#mining").show();
 	$("#portfolio").hide();
+	$("#status").hide();
 }
 
 function portfolio() {
 	$("#dashboard").hide();
 	$("#mining").hide();
 	$("#portfolio").show();
+	$("#status").hide();
+}
+
+function status() {
+	$("#dashboard").hide();
+	$("#mining").hide();
+	$("#portfolio").hide();
+	$("#status").show();
+	$.ajax({
+		type: 'GET',
+		url: '/dashboard/status',
+		contentType: "application/json; charset=utf-8",
+		success: function(data) {
+			console.log(data);
+			data.data.forEach(function(item){
+					var lines = item.split("\n"),
+					output = [],
+					i;
+					for (i = 0; i < lines.length; i++)
+					output.push("<tr><td>"
+					+ lines[i].slice(0,-1).split(",").join("</td><td>")
+					+ "</td></tr>");
+					output = "<table class=\"ui celled padded table\"><tr>"+
+					"<td>Time</td>"+
+					"<td>Card</td>"+
+					"<td>Temp</td>"+
+					"<td>Utilization</td></tr>"+
+					 output.join("") + "</table>";         
+					$("#health").append(output);
+				});
+		},
+		error: function(err) {
+			alert(err);
+		}
+	});
 }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
-
+var Promise = require('bluebird');
+const save = require('save-file');
+var read = require('read-directory');
+var readfiles = require('node-readfiles');
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('dashboard', { title: 'Dashboard' });
+});
+
+router.post('/status-update', function(req, res, next) {
+  console.log(req.body.data);
+  save(req.body.data, '..\\rig-status\\'+req.body.name, (err, data) => {
+      console.log(data);
+      console.log(err);
+      if (err) throw err;
+      res.sendStatus(200);
+      //file is saved at this point, data is arrayBuffer with actual saved data 
+  });
+});
+
+router.get('/status', function(req, res, next) {
+  var dataRead = [];
+  readfiles(__dirname+'\\..\\rig-status\\', {
+    depth: 0
+  }, function (err, filename, content) {
+    if (err) throw err;
+    console.log('File ' + filename + ':');
+    dataRead.push(content);
+  }).then(function (files) {
+    console.log('read '+ dataRead);
+    res.status(200).json({data: dataRead});
+  }).catch(function(err) {
+    res.status(200).json({err: err});
+  });
 });
 
 module.exports = router;
